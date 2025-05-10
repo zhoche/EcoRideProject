908362569ddaf2d085d1156c6ac2eccda0cad664
zhoche <zoehoche@gmail.com>
2025-05-10 22:03:57 +0200

    Ajout du système typographique et correction SCSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }
+];
 
 @NgModule({
   declarations: [],
