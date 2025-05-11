dd5569dd988965d3a444a02dd7279178ca5ab5b6
zhoche <zoehoche@gmail.com>
2025-05-11 10:47:54 +0200

    Routing en cours d'initialisation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // ← redirection par défaut
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomeModule)
+  }
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    AppRoutingModule,
-    AppComponent
+    CommonModule
   ]
 })
 export class AppModule { }
