75afa58964c2bd1b296dadf641e4e53b2a4a06ad
zhoche <zoehoche@gmail.com>
2025-05-13 09:22:32 +0200

    Initialisation du composant search-bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-home',
-  standalone: false,
+  standalone: true,
+  imports: [SearchBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [],
+  standalone: true,
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss'
+})
+export class SearchBarComponent {
+
+}
