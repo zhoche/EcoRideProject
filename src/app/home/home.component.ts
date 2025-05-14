36c176da07fbea7c9642408de5de844918cc7225
zhoche <zoehoche@gmail.com>
2025-05-14 18:49:51 +0200

    bloc features card terminé

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,3 +11,22 @@ import { SearchBarComponent } from '../search-bar/search-bar.component';
 export class HomeComponent {
 
 }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.underline-animate');
+
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  elements.forEach(el => observer.observe(el));
+});
