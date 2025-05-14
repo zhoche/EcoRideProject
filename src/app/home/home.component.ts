2c86f4cf899092f8b04599aa6e38cbdb2584818d
zhoche <zoehoche@gmail.com>
2025-05-14 22:40:52 +0200

    commitment section OK'

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent {
 }
 
 
-
+//ANIMATION SOULIGNEMENT CONFIANCE
 document.addEventListener('DOMContentLoaded', () => {
   const elements = document.querySelectorAll('.underline-animate');
 
@@ -26,6 +26,26 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     },
     { threshold: 0.5 }
+  );
+
+  elements.forEach(el => observer.observe(el));
+});
+
+
+
+//ANIMATION IMAGES SCROLL 
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.animate-on-scroll');
+
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+        }
+      });
+    },
+    { threshold: 0.2 }
   );
 
   elements.forEach(el => observer.observe(el));
