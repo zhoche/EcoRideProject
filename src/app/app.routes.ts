c541a8533f97fc618df4423e85158a9494e433cd
zhoche <zoehoche@gmail.com>
2025-05-17 16:20:43 +0200

    connexion page en cours

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ConnexionModule } from './connexion/connexion.module';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 export const routes: Routes = [
     { path: 'home' , component: HomeComponent },
-    { path: '' , redirectTo: '/home', pathMatch: 'full' }
-];
+    { path: '' , redirectTo: '/home', pathMatch: 'full' },
+    { path: 'connexion' , component: ConnexionComponent },
+]
---
+++ src/app/connexion/connexion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connexion',
+  imports: [],
+  templateUrl: './connexion.component.html',
+  styleUrl: './connexion.component.scss'
+})
+export class ConnexionComponent {
+
+}
