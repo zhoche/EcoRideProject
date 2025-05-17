e23352c583b8f15ac136249f744e7737abe4d7ea
zhoche <zoehoche@gmail.com>
2025-05-17 17:11:25 +0200

    sauvegarde avant ajustements page inscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ConnexionModule } from './connexion/connexion.module';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { InscriptionModule } from './inscription/inscription.module';
 
 export const routes: Routes = [
     { path: 'home' , component: HomeComponent },
     { path: '' , redirectTo: '/home', pathMatch: 'full' },
     { path: 'connexion' , component: ConnexionComponent },
+    { path: 'inscription' , component: InscriptionComponent },
 ]
---
+++ src/app/inscription/inscription.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription',
+  imports: [],
+  templateUrl: './inscription.component.html',
+  styleUrl: './inscription.component.scss'
+})
+export class InscriptionComponent {
+
+}
