9e67c067ee4808a4198db21b2e63e1041f481f85
zhoche <zoehoche@gmail.com>
2025-07-16 14:16:04 +0200

    fix: load routes from config/routes.php to enable attributes

--- backend/src/Kernel.php
+++ backend/src/Kernel.php
@@ -4,8 +4,14 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $routes->import('../config/routes.php');
+    }
 }
