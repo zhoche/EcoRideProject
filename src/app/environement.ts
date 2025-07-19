14ab5b1275ecf0e6cba01b0448f555ea74e9ddf8
zhoche <zoehoche@gmail.com>
2025-07-19 18:00:30 +0200

    modification url back env

--- src/app/environement.ts
+++
@@ -1,4 +0,0 @@
-// export const environment = {
-//     production: false,
-//     orsApiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImFjMDM4YmExNmE4MjQ3M2Q4NTJmYzA1NjYzNmNjNjZiIiwiaCI6Im11cm11cjY0In0='
-// };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ecoride-back-xm7y.onrender.com/api',
+    apiUrl: 'https://ecoride-back-xm7y.onrender.com',
     orsApiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImFjMDM4YmExNmE4MjQ3M2Q4NTJmYzA1NjYzNmNjNjZiIiwiaCI6Im11cm11cjY0In0'
   };
